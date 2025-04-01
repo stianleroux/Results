fdfb1b7ca70fe2dbf41562dca8fffc50dcaad077
Stian <stianleroux@gmail.com>
2025-04-01 21:41:05 +0200

    Cleanup 🧹

--- Results/Models/ResultMapper.cs
+++ Results/Models/ResultMapper.cs
@@ -1,6 +1,7 @@
 ﻿namespace Results.Models;
 
 using Results.Constants;
+using Results.Enums;
 
 public static class ResultMapper
 {
@@ -8,21 +9,21 @@ public static class ResultMapper
         => fromResult switch
         {
             null => Result<T2>.Failure(ErrorConstants.InternalError),
-            { IsError: true } => Result<T2>.Failure(fromResult.Errors, fromResult.Message),
-            { IsNotFound: true } => Result<T2>.NotFound(fromResult.Message),
-            { IsValidationError: true, Data: null } => Result<T2>.ValidationFailure(fromResult.ValidationErrors, fromResult.Message),
-            { IsValidationError: true, Data: not null } => Result<T2>.ValidationFailure(fromResult.ValidationErrors, fromResult.Message, mapper(fromResult.Data)),
-            { Data: not null } => Result<T2>.Success(mapper(fromResult.Data), fromResult.Message),
-            _ => Result<T2>.Success(default, fromResult.Message),
+            { ErrorResult: ErrorResults.GeneralError } => Result<T2>.Failure(fromResult.Errors, fromResult.Message),
+            { ErrorResult: ErrorResults.NotFound } => Result<T2>.NotFound(fromResult.Message),
+            { ErrorResult: ErrorResults.ValidationError, Data: null } => Result<T2>.ValidationFailure(fromResult.ValidationErrors, fromResult.Message),
+            { ErrorResult: ErrorResults.ValidationError, Data: not null } => Result<T2>.ValidationFailure(fromResult.ValidationErrors, fromResult.Message, mapper(fromResult.Data)),
+            { Data: not null } => Result<T2>.Success(mapper(fromResult.Data), fromResult.Count, fromResult.Message),
+            _ => Result<T2>.Success(default, fromResult.Count, fromResult.Message),
         };
 
     public static Result MapToEmptyResult<T1>(Result<T1> fromResult)
         => fromResult switch
         {
             null => Result.Failure(ErrorConstants.InternalError),
-            { IsError: true } => Result.Failure(fromResult.Errors, fromResult.Message),
-            { IsNotFound: true } => Result.NotFound(fromResult.Message),
-            { IsValidationError: true } => Result.ValidationFailure(fromResult.ValidationErrors, fromResult.Message),
+            { ErrorResult: ErrorResults.GeneralError } => Result.Failure(fromResult.Errors, fromResult.Message),
+            { ErrorResult: ErrorResults.NotFound } => Result.NotFound(fromResult.Message),
+            { ErrorResult: ErrorResults.ValidationError } => Result.ValidationFailure(fromResult.ValidationErrors, fromResult.Message),
             _ => Result.Success(fromResult.Message)
         };
 }
